61cde6d54ac4946cd19dda6d990f5e3a614ad327
MakSim345 <senishch@gmail.com>
2019-10-23 14:38:08 +0300

    try turtle framework - make snake game

---
+++ turtle_framework/snake01.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+##
+#############################################################################
+
+import math
+import os, sys
+import traceback
+import turtle
+import time
+from random import randrange
+
+BREAK_FLAG = False
+
+class MyClass():
+    def __init__(self):
+        self.a = 100
+        self.__b = 200
+        
+    def dump(self):    
+        print "My value a = ", self.a
+
+    def dump_b(self):
+        '''test private member'''
+        print "My value __b = ", self.__b
+
+class SnakeGame():
+    def __init__(self):
+        self.screen = turtle.Screen()
+        self.screen.title = "Snake with turtle module"
+        self.screen.bgcolor ('orange')
+        self.screen.setup (650, 650)
+        self.screen.tracer (0)
+
+    def run(self):
+
+        self.screen.mainloop()
+        #while 1:
+        #    sleep(1)
+            #endfor        
+        #endwhile
+            
+if __name__ == "__main__":    
+
+    print "Main program start."
+    print ""
+    print "Python version: "
+    print sys.version
+    print "----------------------\n"
+
+    try:
+        #sm = SnakeGame()
+        #sm.run()        
+        screen = turtle.Screen()
+        screen.title = "Snake with turtle module"
+        screen.bgcolor ('orange')
+        screen.setup (650, 650)
+        screen.tracer (0)
+        # screen.mainloop()
+        turtle.mainloop()
+
+    except NotImplementedError, e:
+        print "1. Error occures:",  e
+    except:
+        print "2. Error occures: unknown"
+        traceback.print_exc()    
+
+
+    print "Main program ends"
+
