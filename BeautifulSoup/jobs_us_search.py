c2f1391515875e2ca25174105fc78ecfa0070a8a
MakSim345 <senishch@gmail.com>
2019-12-10 16:02:50 +0200

    beautifulsoap - edu

--- BeautifulSoup/jobs_us_search.py
+++ BeautifulSoup/jobs_us_search.py
@@ -16,10 +16,12 @@ FILE_NAME = "test.csv"
 def parse(url = URL_TEMPLATE):
     result_list = {'href': [], 'title': [], 'about': []}
     r = requests.get(url)
-    print (r.status_code)
-    return 1
+    # return 1
 
-    soup = bs(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, "html.parser")
+    #print soup
+    #return 1
+
     vacancies_names = soup.find_all('h2', class_='add-bottom-sm')
     vacancies_info = soup.find_all('p', class_='overflow')
     for name in vacancies_names:
@@ -27,6 +29,8 @@ def parse(url = URL_TEMPLATE):
         result_list['title'].append(name.a['title'])
     for info in vacancies_info:
         result_list['about'].append(info.text)
+    print result_list
+
     return result_list
 
 
