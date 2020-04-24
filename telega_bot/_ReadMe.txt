37c1e5879811978b636ae05db0912d111493dab2
MakSim345 <senishch@gmail.com>
2020-04-24 10:24:37 +0300

    Telegramm BOT:         - added weather, news, IP address;         - added time;         - infinite loop without timeout;

--- telega_bot/_ReadMe.txt
+++ telega_bot/_ReadMe.txt
@@ -1,18 +1,24 @@
 2019-AUG
-======
+==============================================================================================
 
 My Telegramm bot.
-======
+==============================================================================================
 
 Weather, temperature in basement, pomodoro, etc...
 
 
-Notes:
+==============================================================================================
+Important!
 
+If the "telebot" was installed with pip, there will be an error:
+> AttributeError: 'TeleBot' object has no attribute 'message_handler'
+
+- Need to install "pyTelegramBotAPI"
 Run PIP update ("sudo" may need):
     >pip install --upgrade pip
 
 Install correct telegramm API:
     >pip install pyTelegramBotAPI
+==============================================================================================
 
 
--- telega_bot/run_bot.py
+++ telega_bot/run_bot.py
@@ -9,23 +9,26 @@
 ##
 #############################################################################
 
+import traceback
+import socket
+import struct
 import time
-import sys
+import sys, os
 import telebot
 
-import json 
-import requests
-
+from platform import system
 from telegram_auth import user_token
-    
+from news_from_izrael import getTheNews
+
+sys.path.insert(0, '/home/ys/dev/scripts/weather_sms')
+from weather_unit import weather_city
+
 u_token = user_token()
 TOKEN = u_token.token
 
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 bot = telebot.TeleBot(TOKEN)
-
-
 
 # @bot.message_handler(commands=['start'])
 # def start_message(message):
@@ -34,9 +37,9 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     # DEBUG: 
-    # print "get_text_message - enter."
+    # print "get_text_message - enter:", message.text.lower()
     if message.text.lower() == "/help":
-        bot.send_message(message.chat.id, "Напиши /start")
+        bot.send_message(message.chat.id, get_help())
     elif message.text.lower()  == "/start":
         bot.send_message(message.chat.id, "Привет, ты написал мне /start")
     elif message.text.lower()  == "time":
@@ -45,9 +48,74 @@ def get_text_messages(message):
         bot.send_message(message.chat.id, 'Привет, мой создатель')
     elif message.text.lower()  == 'Bye':
         bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower()  == 'ip':
+        bot.send_message(message.chat.id, get_ip_address())
+    elif message.text.lower()  == 'news':
+        bot.send_message(message.chat.id, get_news())
+    elif message.text.lower()  == 'whki':
+        bot.send_message(message.chat.id, get_weather('helsinki'))
     else:
         bot.send_message(message.chat.id, "Я тебя не понимаю. Напиши /help.")
 
+def get_help():
+    return "Simple test: /start;\ntime - current timestamp;\nIP - server IP address;\nnews - news from Izrael;\nwhki - weather in Helsinki."
+
+def get_news():
+    text_to_send = getTheNews().encode('utf-8')
+    return text_to_send
+
+def get_local_os():
+    local_os = system()
+    if local_os == 'Linux':
+        retValue = "OS - Linux"
+    elif local_os == 'Windows':
+        retValue = "OS - Windows. Module 'fcntl' not supported!"
+    return retValue
+
+def get_ip_address():
+    ip_addr_list_to_send = "INFO: YS-SERVER " + time.strftime("%H:%M:%S %Y-%m-%d\n") + get_local_os()
+    my_interfaces_list =  os.listdir('/sys/class/net/')
+    for _network_if in my_interfaces_list:
+        _ip_address = select_ip_address(_network_if)
+        ip_addr_list_to_send = ip_addr_list_to_send + '\nIP address ' +_network_if +':\n' + _ip_address
+    #end_for
+
+    print ip_addr_list_to_send
+    return ip_addr_list_to_send
+
+def select_ip_address(ifname):
+    local_os = system()
+    if local_os == 'Linux':
+        import fcntl
+    elif local_os == 'Windows':
+        # print "OS - Windows. Module 'fcntl' not supported!"
+        return socket.gethostbyname(socket.gethostname())
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        retValue = socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15]))[20:24])
+    except:
+        retValue = "IP: "+ifname+" not exists!"
+
+    return retValue
+
+def get_weather(city_param='helsinki'):
+    text_to_send = getTheNews().encode('utf-8')
+    if city_param == 'helsinki':
+        city_url = "http://www.foreca.com/Finland/Helsinki?tenday"
+    else:
+        city_url = "http://www.foreca.com/Ukraine/Dniprodzerzhyns%27k?tenday"
+
+    try:
+        weather = weather_city()
+        weather.set_city_url(city_url)
+        # weather.set_to_russian(False)
+        weather_to_send = weather.get_weather_for_city()
+    except:
+        traceback.print_exc()
+    return weather_to_send
 
 def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
@@ -65,9 +133,10 @@ def get_timestamp(_selector = "date"):
     if _selector == "time":
          return _time
     else:
-        return _date
+        return _date + ' Time: '+ _time
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.infinity_polling(True)
+    # bot.polling(none_stop=True)
     #bot.polling()
 
